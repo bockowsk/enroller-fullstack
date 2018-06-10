5f18a590af08af39fd8d4f2ebbcde3b60599dcf8
Wojciech Frącz <fraczwojciech@gmail.com>
2018-06-10 23:05:39 +0200

    GR 1 ready

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -45,6 +45,15 @@
             },
             logout() {
                 this.authenticatedUsername = '';
+            },
+            register(user) {
+                this.$http.post('participants', user)
+                    .then(response => {
+                        // udało się
+                    })
+                    .catch(response => {
+                        // nie udało sie
+                    });
             }
         }
     };
