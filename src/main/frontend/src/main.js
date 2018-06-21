ee23df8dc79c1923a7118f88b0b6159e3b7f0746
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-06-21 12:56:42 +0200

    dependencies added

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+Vue.http.options.root = '/api';
+Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
+
 new Vue({
     el: '#app',
+    http: {
+    	root: '/root',
+    	headers: {
+    		Authorization: 'Basic YXBpOnBhc3N3b3Jk'
+        }
+    },
     render: h => h(App)
 });
