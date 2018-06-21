9e52f3211610b322bdf578bd65dc069cf0aa48e1
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-06-21 14:32:04 +0200

    vue App fixed

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
+import VueResource from "vue-resource";
 
+Vue.use(VueResource);
 Vue.http.options.root = '/api';
 Vue.http.headers.common['Authorization'] = 'Basic YXBpOnBhc3N3b3Jk';
 
