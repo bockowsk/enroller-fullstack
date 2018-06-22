28dfe2a31a55e96091cec2a07b59b2a7e66e1385
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-06-22 10:37:47 +0200

    token working properly

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -2,6 +2,8 @@ package com.company.enroller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class App {
@@ -9,4 +11,10 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+    
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(15);
+		return bCryptPasswordEncoder;
+	}
 }
--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,26 +1,44 @@
 package com.company.enroller.security;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-   @Override
-   protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable()
-               .authorizeRequests()
-               .anyRequest().permitAll()
-               .and()
-               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-   }
-   
-   @Bean
-   public BCryptPasswordEncoder passwordEncoder() {
-	   BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(15);
-	   return bCryptPasswordEncoder;
-   }
+
+	@Autowired
+	ParticipantProvider participantProvider;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	@Value("${security.secret}")
+	private String secret;
+	@Value("${security.issuer}")
+	private String issuer;
+	@Value("${security.token_expiration_in_seconds}")
+	private int tokenExpiration;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and()
+		.addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret, issuer, tokenExpiration), UsernamePasswordAuthenticationFilter.class)
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				
+				;
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
+	}
+
+
 }
